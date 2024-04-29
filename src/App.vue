2781373dcd7346e37d6fa1089cfc394bf3213168
HUJING <2046201531@qq.com>
2024-04-29 11:40:44 +0800

    echarts焦点错位问题解决

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,33 @@
       <el-button type="warning">警告按钮</el-button>
       <el-button type="danger">危险按钮</el-button>
     </el-row> -->
-    <router-view ></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  
-}
+  name: "App",
+};
 </script>
 
 <style lang="less" >
-html,body,p {
+html,
+body,
+p {
   margin: 0;
   padding: 0;
+}
+// 定义浏览器默认67%的比例缩放
+body {
+  zoom: 0.67;
+}
+//由于zoom缩放会导致echarts图表焦点错位，所以需要在canvas中进行调整
+canvas {
+  //zoom: 1/缩放比例
+  zoom: 1.49;
+  transform: scale(0.67);
+  transform-origin: 0px 0px;
 }
 </style>
 
